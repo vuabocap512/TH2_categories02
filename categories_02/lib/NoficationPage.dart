b551325fd09f05f34a49917b0d8043913573021e
vuabocap512 <125243807+vuabocap512@users.noreply.github.com>
2023-05-09 22:36:30 +0700

    Update NoficationPage.dart

--- categories_02/lib/NoficationPage.dart
+++ categories_02/lib/NoficationPage.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
 
-class NoficationPage extends StatefulWidget {
-  const NoficationPage({super.key});
-
-  @override
-  State<NoficationPage> createState() => _NoficationPageState();
-}
-
-class _NoficationPageState extends State<NoficationPage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('notification'),
-    );
-  }
-}
